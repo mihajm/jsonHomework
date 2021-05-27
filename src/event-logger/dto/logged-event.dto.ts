17ae4d13f3831fbf86ee353650c1c2f74134facf
mihajm <mihavenxly@gmail.com>
2021-05-27 15:16:57 +0200

    import cleanup

--- src/event-logger/dto/logged-event.dto.ts
+++ src/event-logger/dto/logged-event.dto.ts
@@ -2,12 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsOptional, IsString } from 'class-validator';
 
 export class CreateLoggedEventDto {
-  /*@ApiProperty({
-    description: 'The dateTime object representing when the event began',
-    example: '2021-05-27T09:02:36.526Z',
-  })
-  @IsDate()
-  startTime: Date;*/
   @ApiProperty({
     description: 'Reference to the execution context of the log',
     example: 'doctorService',
